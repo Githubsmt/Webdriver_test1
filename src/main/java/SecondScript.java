479abb8f55ddcaa7e50954c730725f1296f0e51c
Smita_wbdriver_test <currentwebwork@gmail.com>
2023-07-02 17:23:09 +0530

    First_case

---
+++ src/main/java/SecondScript.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SecondScript {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.in/");
+        driver.manage().window().maximize();
+        String title = driver.getTitle();
+        if (title.equals("Google")){
+            System.out.println("Title is correct");
+        }
+        else {
+            System.out.println("Title is incorrect");
+        }
+        //driver.quit();
+    }
+
+}
+
